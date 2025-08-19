2ec87569609a845b8e1a72df5a043fa4ee94dbe9
mchen <mchennamartin@maine.edu>
2025-08-19 11:22:07 -0400

    improves comment readability

--- R/initializing/packages/script.R
+++ R/initializing/packages/script.R
@@ -1,32 +1,33 @@
-#function I made, to install and load packages from CRAN and Bioconductor at the same time, no user input
-#makes it much easier when changing R versions, correct package versions are automatically downloaded
+# function I made, to install and load packages from CRAN and Bioconductor at the same time, no user input
+# makes it much easier when changing R versions, correct package versions are automatically downloaded
 
-#function loads listed package and downloads if needed,
-#I will eventually make it work with a list of functions in later versions
+# function loads listed package and downloads if needed,
+# I will eventually make it work with a list of functions in later versions
 
+# Function --------------------------------------------------------------------
 load <- function(package){
-  #package - character - represents package or packages to install e.g. 'dplyr', 'limma'
+  # package - character - represents package or packages to install e.g. 'dplyr', 'limma'
 
-  #determine whether it is one package or a list of them, by detecting commas
+  # determine whether it is one package or a list of them, by detecting commas
   checkList <- grepl(', ', package)
-  #if package variable has commas, checkList will return True
+  # if package variable has commas, checkList will return True
 
-  #for listed input
+  # for listed input
   if(checkList == TRUE){
-    #create list type from character/string
+    # create list type from character/string
     packageList <- strsplit(package, ', ')
     packageList <- packageList[[1]]
 
-    #look at number of elements in packageList
+    # look at number of elements in packageList
     packageNumber <- length(packageList)
 
-    #create loop with length equal to number of packages
+    # create loop with length equal to number of packages
     for(i in 1:packageNumber){
       if(!require(packageList[[i]], character.only = TRUE)){
         install.packages(packageList[[i]], ask = FALSE)
       }
-      #check to see if package installed, or if there was an error.
-      #if packages is not installed yet, repeat with biocmanager
+      # check to see if package installed, or if there was an error.
+      # if packages is not installed yet, repeat with biocmanager
       if(!require(packageList[[i]], character.only = TRUE)){
         if (!require("BiocManager", quietly = TRUE))
         install.packages("BiocManager")
@@ -34,19 +35,19 @@ load <- function(package){
         BiocManager::install(packageList[[i]], ask = FALSE)
        }
 
-    #load package into environment
+    # load package into environment
     library(packageList[[i]], character.only = TRUE)
     }
     }
 
 
-  #for sole input
+  # for sole input
   if(checkList == FALSE){
      if(!require(package, character.only = TRUE)){
         install.packages(package, ask = FALSE)
       }
-      #check to see if package installed, or if there was an error.
-      #if packages is not installed yet, repeat with biocmanager
+      # check to see if package installed, or if there was an error.
+      # if packages is not installed yet, repeat with biocmanager
       if(!require(package, character.only = TRUE)){
         if (!require("BiocManager", quietly = TRUE))
         install.packages("BiocManager")
@@ -54,14 +55,14 @@ load <- function(package){
         BiocManager::install(package, ask = FALSE)
        }
 
-    #load package into environment
+    # load package into environment
     library(package, character.only = TRUE)
     }
 
   }
 
-
-#now install/load packages with one line:
+# Test -------------------------------------
+# now install/load packages with one line:
 load('dplyr')
 
 packageList <- 'limma, dplyr'
--- R/plots/boxplots/script.R
+++ R/plots/boxplots/script.R
@@ -1,19 +1,20 @@
-#Simple Boxplot -----------------------------------------------------
-#boxplot function that quicky makes a plot from a qualitative independent variable and quantitative dependent
-#lazy way to check out different relationships without taking up too much time or space
+# Simple Boxplot -----------------------------------------------------
 
-#package requirements: ggplot2, dplyr, ggpubr
+# boxplot function that quicky makes a plot from a qualitative independent variable and quantitative dependent
+# lazy way to check out different relationships without taking up too much time or space
 
-#table should be in normal tidy format
+# package requirements: ggplot2, dplyr, ggpubr
+
+# table should be in normal tidy format
 
 simpleBoxplot <- function(table, independent, dependent){
-  #This function creates a boxplot using an independent and dependent variable
-  #Uses a table and two columns from the table to create the boxplot
-  #input independent and dependent variables as strings E.g 'size','volume'
+  # This function creates a boxplot using an independent and dependent variable
+  # Uses a table and two columns from the table to create the boxplot
+  # input independent and dependent variables as strings E.g 'size','volume'
 
-  #Independent = string representing column with numeric values
-  #Dependent = String representing table column with qualitative values
-  #table = data frame
+  # Independent = string representing column with numeric values
+  # Dependent = String representing table column with qualitative values
+  # table = data frame
   p <- ggboxplot(table, x=independent,y=dependent,
                  color=independent, palette =c("#00AFBB", "#E7B800", '#FF7F00'),
                  add="jitter",shape=independent)
